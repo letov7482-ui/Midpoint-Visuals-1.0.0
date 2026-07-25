28e26953817faa753eecde513b4200bc4ed4c0a9
letov7482-ui <letov7482@gmail.com>
2026-07-25 15:19:49 +0300

    Create MixinMinecraftClient.java

---
+++ src/main/java/me/midpoint/mixin/MixinMinecraftClient.java
@@ -0,0 +1,20 @@
+package me.midpoint.mixin;
+
+import me.midpoint.MidpointClient;
+import me.midpoint.modules.BaseModule;
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftClient.class)
+public class MixinMinecraftClient {
+    @Inject(method = "tick", at = @At("HEAD"))
+    private void onTick(CallbackInfo ci) {
+        if (MinecraftClient.getInstance().player == null) return;
+        for (BaseModule m : MidpointClient.modules) {
+            if (m.enabled) m.tick();
+        }
+    }
+}

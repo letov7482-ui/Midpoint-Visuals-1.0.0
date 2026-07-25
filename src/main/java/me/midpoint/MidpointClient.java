044fd6ac53d4bdb0296b4220e926b2da1f6df30b
letov7482-ui <letov7482@gmail.com>
2026-07-25 14:52:52 +0300

    Update MidpointClient.java

--- src/main/java/me/midpoint/MidpointClient.java
+++ src/main/java/me/midpoint/MidpointClient.java
@@ -5,6 +5,6 @@ import net.fabricmc.api.ClientModInitializer;
 public class MidpointClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        System.out.println("[Midpoint Visuals] Загружен!");
+        System.out.println("Midpoint Visuals loaded successfully!");
     }
 }

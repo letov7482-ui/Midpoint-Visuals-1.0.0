5020e853c4e884d05c4e8cca4ca6e710124a6652
letov7482-ui <letov7482@gmail.com>
2026-07-25 14:40:36 +0300

    Update MidpointClient.java

--- src/main/java/me/midpoint/MidpointClient.java
+++ src/main/java/me/midpoint/MidpointClient.java
@@ -1,70 +1,22 @@
 package me.midpoint;
 
-import me.midpoint.config.ModConfig;
-import me.midpoint.modules.*;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@Environment(EnvType.CLIENT)
 public class MidpointClient implements ClientModInitializer {
-    public static final String MOD_ID = "midpoint";
-    public static ModConfig CONFIG;
-    public static List<Module> modules = new ArrayList<>();
     public static KeyBinding openGuiKey;
 
     @Override
     public void onInitializeClient() {
-        CONFIG = ModConfig.load();
-        
-        // Регистрация модулей
-        modules.add(new EmoteModule());
-        modules.add(new ShiftTapModule());
-        modules.add(new HealingHelperModule());
-        modules.add(new CooldownsModule());
-        modules.add(new ItemScrollerModule());
-        modules.add(new ItemSwapModule());
-        modules.add(new ElytraSwapModule());
-        modules.add(new TotemTrackerModule());
-        modules.add(new FreeLookModule());
-        modules.add(new AspectRatioModule());
-        modules.add(new BlockOverlayModule());
-        modules.add(new FullbrightModule());
-        modules.add(new HitboxCustomizerModule());
-        modules.add(new ParticlesModule());
-        modules.add(new TargetESPModule());
-        modules.add(new WorldParticlesModule());
-        modules.add(new NimbModule());
-        modules.add(new HudModule());
-        modules.add(new PotionsModule());
-        modules.add(new TargetHudModule());
-        modules.add(new WatermarkModule());
-
-        // Клавиша для открытия GUI (по умолчанию P)
+        // Регистрация клавиши для открытия GUI (позже)
         openGuiKey = KeyBindingHelper.registerKeyBinding(
             new KeyBinding("key.midpoint.openGui", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_P, "category.midpoint.general")
         );
 
-        // Tick обработчик
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (client.player == null || client.world == null) return;
-            for (Module m : modules) {
-                if (m.enabled) m.tick();
-            }
-        });
-
-        System.out.println("[Midpoint Visuals] Загружено!");
-    }
-
-    public static void tickModules() {
-        // Вызывается из миксина для рендера
+        System.out.println("[Midpoint Visuals] Загружен (базовая версия без модулей)");
+        System.out.println("[Midpoint Visuals] Модули будут добавлены позже.");
     }
 }
